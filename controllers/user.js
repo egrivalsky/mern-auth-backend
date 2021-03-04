26beb0cb4255cd2a29c59a119ab2040386db3d37
unknown <erik@computer.com>
2021-03-04 15:53:25 -0700

    login route

--- controllers/user.js
+++ controllers/user.js
@@ -21,7 +21,7 @@ const register = (req, res) => {
         if (user) {
             return res.status(400).json({ message: 'email already exists' })
         } else {
-            const newUser = new User({
+            const newUser = new db.User({
                 name: req.body.name,
                 email: req.body.email,
                 password: req.body.password
@@ -43,9 +43,52 @@ const register = (req, res) => {
 
         }
     })
+    .catch(err => console.log('Error finding user:', err))
+}
+
+const login = async (req, res) => {
+    console.log('=====> inside of /login');
+    console.log('====> /login -> req.body');
+    console.log(req.body);
+
+    const foundUser = await db.User.findOne({ email: req.body.email});
+
+    if (foundUser) {
+        // user is in the DB
+        let isMatch = await bcrypt.compare(password, foundUser.password);
+        console.log(isMatch);
+        if (isMatch) {
+            // if user match, then we want to send a JSON Web Token
+            // Create a token payload
+            // add an expiredToken = Date.now()
+            // save the user
+            const payload = {
+                id: foundUser.id,
+                email: foundUser.email,
+                name: foundUser.name
+            }
+            jwt.sign(payload, JWT_SECRET, { expiresIn: 3600 }, (err, token) => {
+                if (err) {
+                    res.status(400).json({ message: 'Session has ended, please log in again'});
+                }
+                const legit = jwt.verify(token, JWT_SECRET, { expiresIn: 60 });
+                console.log('===> legit');
+                console.log(legit);
+                res.json({ success: true, token: `Bearer ${token}`, userData: legit});
+            });
+
+            
+        } else {
+            return res.status(400).json({message: 'Email or password is incorrect' });
+        }
+    } else {
+        return res.status(400).json({message: 'User not found'})
+    }
 }
 
 //Exports
 module.exports = {
     test,
+    register,
+    login,
 }
